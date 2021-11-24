8af95f0df75c43c5b9133f4c23777d740d54f68b
manoharuss <manoharuss@gmail.com>
2021-11-24 13:10:08 +0530

    Add consumer to print key/value

---
+++ run_consumer.py
@@ -0,0 +1,21 @@
+from kafka import KafkaConsumer
+
+if __name__ == '__main__':
+    print('Running Consumer..')
+    parsed_records = []
+    topic_name = 'openstreetmap-minutely'
+
+    consumer = KafkaConsumer(topic_name, auto_offset_reset='earliest',
+                             bootstrap_servers=['localhost:9092'], api_version=(0, 10), consumer_timeout_ms=1000)
+    for published_msg in consumer:
+        # This is the OpenStreetMap minutely osc text
+        consume_text = published_msg.value
+        # This is the sequenece number that belongs to the OpenStreetMap minutely diff
+        consume_sequence = published_msg.key
+        # TODO: We can do anything with the key/value pairs from here.
+        print(f"Consumed sequence number {consume_sequence}")
+
+    print("Consumer ended after reading all messages successfully.")
+
+    consumer.close()
+
